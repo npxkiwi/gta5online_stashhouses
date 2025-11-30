bf030e8871901888bfae82d8d2d1fe25cd28af1e
Valder <151445483+npxkiwi@users.noreply.github.com>
2025-11-30 19:49:48 +0100

    Add props.lua with sticky note codes

---
+++ props.lua
@@ -0,0 +1,46 @@
+local shared = {}
+
+shared.Codes = { -- All the stickynote codes that it can be. Do not change this
+    [1] = {
+        Code = {28, 03, 98},
+        Prop = 'xm3_prop_xm3_code_28_03_98'
+    },
+    [2] = {
+        Code = {01, 23, 45},
+        Prop = 'xm3_prop_xm3_code_01_23_45'
+    },
+    [3] = {
+        Code = {44, 23, 37},
+        Prop = 'xm3_prop_xm3_code_44_23_37'
+    },
+    [4] = {
+        Code = {77, 79, 73},
+        Prop = 'xm3_prop_xm3_code_77_79_73'
+    },
+    [5] = {
+        Code = {02, 12, 87},
+        Prop = 'xm3_prop_xm3_code_02_12_87'
+    },
+    [6] = {
+        Code = {05, 02, 91},
+        Prop = 'xm3_prop_xm3_code_05_02_91'
+    },
+    [7] = {
+        Code = {28, 11, 97},
+        Prop = 'xm3_prop_xm3_code_28_11_97'
+    },
+    [8] = {
+        Code = {24, 10, 81},
+        Prop = 'xm3_prop_xm3_code_24_10_81'
+    },
+    [9] = {
+        Code = {72, 68, 83},
+        Prop = 'xm3_prop_xm3_code_72_68_83'
+    },
+    [10] = {
+        Code = {73, 27, 38},
+        Prop = 'xm3_prop_xm3_code_73_27_38'
+    },
+}
+
+return shared
